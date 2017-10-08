df7dab1262602d911beae277efc2463cb396e29b
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2017-10-07 23:23:48 -0700

    playing with oppening animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-var $logo = $('#lasimaLogo')
+var $logo = $('#lasimaLogo');
+var $door1 = $('#door1');
+var $door2 = $('#door2');
 
 $logo.click(function() {
-	$logo.animate({left: '250px'});
-})
+	$logo.fadeOut(3000);
+	$door1.animate({"left": "-=49%"}, 3000);
+	$door2.animate({"left": "+=49%"}, 3000);
+});
