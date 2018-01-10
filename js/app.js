41f1029825740fb08c8e6f180a945e1cdab0053b
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2018-01-10 12:37:18 -0800

    updates the ui, adds pparalax and sticky nav

--- js/app.js
+++ js/app.js
@@ -2,8 +2,27 @@ var $logo = $('#lasimaLogo');
 var $door1 = $('#door1');
 var $door2 = $('#door2');
 
+// Oppening animation set up
+
 $logo.click(function() {
 	$logo.fadeOut(3000);
 	$door1.animate({"left": "-=49%"}, 3000);
 	$door2.animate({"left": "+=49%"}, 3000);
 });
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
